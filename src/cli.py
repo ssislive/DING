979ffcd7aa13c4414ae0bd9dc941c13673f3dc88
Sujal Srivastava <sujal.srivastava29@gmail.com>
2025-12-29 19:21:31 +0530

    Implement ding init command

--- src/cli.py
+++ src/cli.py
@@ -1,14 +1,23 @@
+import argparse
+from src import data
+
+def init(args):
+    data.init()
+
+def parse_args():
+    parser= argparse.ArgumentParser(prog="ding")
+
+    commands= parser.add_subparsers(dest="command", required=True)
+
+    init_parser = commands.add_parser("init")
+    init_parser.set_defaults(func=init)
+
+    return parser.parse_args()
+
+
 def main():
-    print("Ding Dong, who's there \nCash\n Cash who?\n I prefer peanuts\n")
+    print("Ding Dong, who's there \nThe IRS \nOh Shit")
 
-from src.data import init
 
-import sys
-
-def main():
-    if len(sys.argv) < 2:
-        print("Usage: ding <command>")
-        return
-
-    if sys.argv[1] == "init":
-        init()
+if __name__ == "__main__":
+    main()
